2459875fa7839bb36a41a2fc73e666fd280a8325
Jitendra Patel <41681183+ammipatel@users.noreply.github.com>
2020-10-07 09:56:35 +0530

    [Jitendra] added; Method for clearify whcih palyer is play

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -1,17 +1,40 @@
 import java.util.*;
 public class TicTacToeGame{
-	public static void create_game_Board()
+	static String turn;
+	public static char[] createBoard()
 	{
 		char[] gameBoard=new char[10];
 		for(int i=1;i<10;i++)
 		{
 			gameBoard[i]=' ';
 		}
-			
+		return gameBoard;	
+	}
+	public static void playerplayfirst()
+	{
+		System.out.println("Write the play key");
+		Scanner sc1=new Scanner(System.in);
+		turn=sc1.nextLine();
+		if(turn.equals("X"))
+		{
+			System.out.println("Computer is playing");
+		}
+		else if(turn.equals(0))
+		{
+			System.out.println("User is playing");
+		}
+		sc1.close();
 	}
 	public static void main(String[] args)
 	{
-		create_game_Board();
+		Scanner sc=new Scanner(System.in);
+		char[] board=createBoard();
+		System.out.println("Welocme to User And Computer Player");
+		playerplayfirst();
+		
+		
+		
+		
 		
 	}
 }

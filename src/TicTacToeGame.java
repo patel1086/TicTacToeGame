eca98f4e320efa29bcba519d75c4d0f862235198
Jitendra Patel <41681183+ammipatel@users.noreply.github.com>
2020-10-07 10:37:55 +0530

    [Jitendra] Added : Show game board of tic tok toe game

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -10,34 +10,39 @@ public class TicTacToeGame{
 		}
 		return gameBoard;	
 	}
-	public static String chooseplayer()
+	public static String choosePlayerTurn()
 	{
 		System.out.println("Write the play key");
-		Scanner sc1=new Scanner(System.in);
-		turn=sc1.nextLine(); // X is for computer player and O for User player
+		Scanner sc=new Scanner(System.in);
+		turn=sc.nextLine(); // X is for computer player and O for User player
 		if(turn.equals("X"))
 		{
 			System.out.println("Computer is playing");
-			turn="O";
+			turn="O";      // Computer is playing so now it is turn to play user
 			return turn;
 		}
 		else 
 		{
 			System.out.println("User is playing");
-			turn="X";
+			turn="X";     // User is playing so now it is turn to play computer
 			return turn;
 		}
 	}
+	public static void showBoard(char [] board)
+	{
+		System.out.println("/---|---|---\\");
+		System.out.println("| " + board[1] + " | " + board[2] + " | " + board[3] + " |");
+		System.out.println("|-----------|");
+		System.out.println("| " + board[4] + " | " + board[5] + " | " + board[6] + " |");
+		System.out.println("|-----------|");
+		System.out.println("| " + board[7] + " | " + board[8] + " | " + board[9] + " |");
+		System.out.println("/---|---|---\\");
+	}
 	public static void main(String[] args)
 	{
-		Scanner sc=new Scanner(System.in);
-		char[] board=createBoard();
+		char[] board=createBoard(); //Method for creating Empty board for game
 		System.out.println("Welocme to User And Computer Player");
-		String turn=chooseplayer();
-		
-		
-		
-		
-		
-	}
+		String turn=choosePlayerTurn(); //Method for choose player turn
+		showBoard(board);
+    }
 }

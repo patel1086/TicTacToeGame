78cf06ef18cc1309e64fdc21d8d4150234a16c48
Jitendra Patel <41681183+ammipatel@users.noreply.github.com>
2020-10-07 09:26:15 +0530

    [Jitendra] Added : Create GameBoard of Char size 10 and initialize empty

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -1,10 +1,17 @@
 import java.util.*;
 public class TicTacToeGame{
-	
-	public static void main(String[] args)
+	public static void create_game_Board()
 	{
 		char[] gameBoard=new char[10];
-		Arrays.fill(gameBoard, ' ');
+		for(int i=1;i<10;i++)
+		{
+			gameBoard[i]=' ';
+		}
+			
+	}
+	public static void main(String[] args)
+	{
+		create_game_Board();
 		
 	}
 }
